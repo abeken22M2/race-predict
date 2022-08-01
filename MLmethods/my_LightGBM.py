5371b2adf4f3342ee73910d96ae5eb7503b9ac04
komoroko <pluskomoroko@gmail.com>
2022-08-01 10:07:07 +0900

    回帰モデルの追加，それに伴い，作成する学習データに"goal_time"を追加したため，使用しない場合は，削除する必要あり．

--- MLmethods/my_LightGBM.py
+++ MLmethods/my_LightGBM.py
@@ -29,15 +29,15 @@ def my_lightgbm(X_train, y_train, X_valid, y_valid, num_class):
         "verbosity": 0,
         "feature_pre_filter": False,
         "force_col_wise": True,
-        "metric" : "None",
+        #"metric" : "None",
         'feature_pre_filter': False, 
-        'lambda_l1': 0.0, 
-        'lambda_l2': 0.0, 
-        'num_leaves': 132, 
-        'feature_fraction': 0.5, 
-        'bagging_fraction': 0.5045608972476101, 
-        'bagging_freq': 4, 
-        'min_child_samples': 20, 
+        'lambda_l1': 2.509352219435235e-07, 
+        'lambda_l2': 0.00017221158210611775, 
+        'num_leaves': 242, 
+        'feature_fraction': 0.8, 
+        'bagging_fraction': 1.0, 
+        'bagging_freq': 0, 
+        'min_child_samples': 5, 
         'num_iterations': 10000,
         'early_stopping_round': 80,
     }
@@ -58,6 +58,7 @@ def optuna_lightgbm(X_train, y_train, X_valid, y_valid, num_class):
     eval_error = lambda x,y: focal_loss_lgb_eval_error(x, y, 0.25, 1., num_class)
 
     params = {
+        "objective": "multiclass", # custom objectiveを使う場合は，宣言しない．
         "num_class": num_class,
         "early_stopping_round": 80,
         "learning_rate": 0.005,
@@ -66,12 +67,14 @@ def optuna_lightgbm(X_train, y_train, X_valid, y_valid, num_class):
         "metric": "multi_logloss",
         'num_iterations': 10000,
     }
-    opt = optuna_lgb.train(params, lgb_train, valid_sets=eval_data, fobj=focal_loss, feval=eval_error)
+    #opt = optuna_lgb.train(params, lgb_train, valid_sets=eval_data, fobj=focal_loss, feval=eval_error)
+    opt = optuna_lgb.train(params, lgb_train, valid_sets=eval_data)
 
     # チューニングしたパラメータで，モデル作成
     print(f"調整したパラメータ\t{opt.params}")
     
-    model = lgb.train(opt.params, lgb_train, valid_sets=eval_data, fobj=focal_loss, feval=eval_error)
+    #model = lgb.train(opt.params, lgb_train, valid_sets=eval_data, fobj=focal_loss, feval=eval_error)
+    model = lgb.train(opt.params, lgb_train, valid_sets=eval_data)
 
     return model
 
@@ -98,3 +101,59 @@ def focal_loss_lgb_eval_error(y_pred, dtrain, alpha, gamma, num_class):
     p = 1/(1+np.exp(-y_pred))
     loss = -( a*y_true + (1-a)*(1-y_true) ) * (( 1 - ( y_true*p + (1-y_true)*(1-p)) )**g) * ( y_true*np.log(p)+(1-y_true)*np.log(1-p) )
     return 'focal_loss', np.mean(loss), False
+
+
+
+def my_lightgbm_regression(X_train, y_train, X_valid, y_valid):
+    lgb_train = lgb.Dataset(np.array(X_train), label=np.array(y_train))
+    eval_data = lgb.Dataset(np.array(X_valid), np.array(y_valid), reference=lgb_train)
+
+    # LightGBM のハイパーパラメータ
+    params = {
+        "objective": "regression",
+        "metric" : "rmse",
+        "learning_rate": 0.005,
+        "verbosity": 0,
+        "feature_pre_filter": False,
+        "force_col_wise": True,
+        'feature_pre_filter': False, 
+        'lambda_l1': 2.509352219435235e-07, 
+        'lambda_l2': 0.00017221158210611775, 
+        'num_leaves': 242, 
+        'feature_fraction': 0.8, 
+        'bagging_fraction': 1.0, 
+        'bagging_freq': 0, 
+        'min_child_samples': 5, 
+        'num_iterations': 10000,
+        'early_stopping_round': 80,
+    }
+
+    # 上記のパラメータでモデルを学習する
+    model = lgb.train(params, lgb_train, valid_sets=eval_data)
+
+    print("Params: ", model.params)
+
+    return model
+
+
+def optuna_lightgbm_regression(X_train, y_train, X_valid, y_valid):
+    lgb_train = lgb.Dataset(np.array(X_train), label=np.array(y_train))
+    eval_data = lgb.Dataset(np.array(X_valid), np.array(y_valid), reference=lgb_train)
+
+    params = {
+        "objective": "regression",
+        "metric" : "rmse",
+        "early_stopping_round": 80,
+        "learning_rate": 0.005,
+        "verbosity": -1,
+        "force_col_wise": True,
+        'num_iterations': 10000,
+    }
+    opt = optuna_lgb.train(params, lgb_train, valid_sets=eval_data)
+
+    # チューニングしたパラメータで，モデル作成
+    print(f"調整したパラメータ\t{opt.params}")
+    
+    model = lgb.train(opt.params, lgb_train, valid_sets=eval_data)
+
+    return model
